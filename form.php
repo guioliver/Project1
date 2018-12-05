2caeb4d0de584b5bdf4c63571d12b83d9184e1d4
guioliver <44738532+guioliver@users.noreply.github.com>
2018-12-05 21:13:55 -0200

    Update form.php

--- form.php
+++ form.php
@@ -1,13 +1,17 @@
 <?php
 $Nome=$_POST["nome"];
-$Endereço=$_POST["endereço"];
+$Endereco=$_POST["endereço"];
 $Data=$_POST["data"];
 $Email=$_POST ["email"];
 $Sexo=$_POST["sexo"];
 
 echo 'Nome: ' ($_POST["nome"] ?? ''); "<br>";
-echo 'Endereço: ' ($_POST["endereço"] ?? ''); "<br>"
+echo 'Endereco: ' ($_POST["endereco"] ?? ''); "<br>"
 echo 'Endereço: ' ($_POST["endereço"] ?? '');  "<br>"
+echo 'Data: ' ($_POST["data"] ?? '');  "<br>"
+echo 'Email: ' ($_POST["email"] ?? '');  "<br>"
+echo 'Sexo: ' ($_POST["sexo"] ?? '');  "<br>"
+  
  ;
  ;
 
